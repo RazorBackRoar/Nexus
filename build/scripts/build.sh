bba2f41a7e4133d453ed2ae7f8cfdd347aa9fe9a
RazorBackRoar <184320956+RazorBackRoar@users.noreply.github.com>
2025-12-20 04:14:01 -0700

    feat: Increase DMG window dimensions and adjust icon vertical positions.

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -92,12 +92,12 @@ tell application "Finder"
         set current view of container window to icon view
         set toolbar visible of container window to false
         set statusbar visible of container window to false
-        set the bounds of container window to {200, 200, 740, 520}
+        set the bounds of container window to {200, 200, 740, 560}
         set theViewOptions to the icon view options of container window
         set arrangement of theViewOptions to not arranged
         set icon size of theViewOptions to 100
-        set position of item "${APP_NAME}.app" of container window to {140, 130}
-        set position of item "Applications" of container window to {400, 130}
+        set position of item "${APP_NAME}.app" of container window to {140, 160}
+        set position of item "Applications" of container window to {400, 160}
         update without registering applications
         delay 1
         close
